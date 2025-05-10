031d771577a0d6f512a89c6352c7f6d9997facc0
Henrique <henrique.maiacardosa@gmail.com>
2025-05-10 19:02:51 -0300

    Corrige importação das rotas no server.js

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+// Importando as rotas
+import userRoutes from "./src/routes/userRoutes.js";
+import goalRoutes from "./src/routes/goalRoutes.js";
+import commentRoutes from "./src/routes/commentRoutes.js";
+import incentiveRoutes from "./src/routes/incentiveRoutes.js";
+import authRoutes from "./src/routes/authRoutes.js";
+import transactionRoutes from "./src/routes/transactionRoutes.js";
+
 dotenv.config();
 
 const app = express();
@@ -11,12 +19,13 @@ const corsOptions = {
   origin: ["http://localhost:3000", "https://goals-eta.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true, 
+  credentials: true, // Permitir envio de cookies e credenciais
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
+// Rotas da API
 app.use("/api/usuarios", userRoutes);
 app.use("/api/metas", goalRoutes);
 app.use("/api/comentarios", commentRoutes);
